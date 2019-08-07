bd8d25e96dbfe7745503a864090ad5c4a8cb2e7e
Anirudh Sreeram <47698070+AnirudhSreeram@users.noreply.github.com>
2019-08-07 11:56:39 +0530

    Update DNN_notebook2.py

--- DNN_notebook2.py
+++ DNN_notebook2.py
@@ -53,3 +53,91 @@ dim = 2
 w, b = initialize_with_zeros(dim)
 print ("w = " + str(w))
 print ("b = " + str(b))
+
+def propagate(w, b, X,Y):
+    m = X.shape[1]
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = (-1 / m)* np.sum(Y*np.log(A) + (1-Y)*(np.log(1-A)))
+
+    dw = (1 / m )*np.dot(X,(A-Y).T)
+    db = (1 / m ) * np.sum(A-Y)
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = True):
+    costs = []
+    for i in range(num_iterations):
+        grads, cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+        if i % 100 ==0:
+            costs.append(cost)
+        if print_cost and i % 100 ==0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+        params = {"w": w,
+              "b": b}
+
+        grads = {"dw": dw,
+                 "db": db}
+
+    return params, grads, costs
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0],1)
+    A = sigmoid(np.dot(w.T,X) + b)
+    for i in range(A.shape[1]):
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+    assert(Y_prediction.shape == (1, m))
+    return Y_prediction
+w = np.array([[0.1124579],[0.23106775]])
+b = -0.3
+X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+print ("predictions = " + str(predict(w, b, X)))
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = True):
+    w , b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost = True)
+    w = parameters["w"]
+    b = parameters["b"]
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train" : Y_prediction_train,
+         "w" : w,
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
